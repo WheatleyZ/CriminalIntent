8f4fc5546aed07cee6ba98883bf7d046e7f2b37d
WheatleyZ <zhengsl7749@live.com>
2016-10-08 15:48:09 +0800

    update

--- app/src/main/java/com/example/criminalintent/Crime.java
+++ app/src/main/java/com/example/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -10,10 +11,31 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
+
     }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean getSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
 
     public UUID getId() {
         return mId;
